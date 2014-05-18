78e1e0bc56a3ca907313e406bb7f0fd5f175ecf6
ngcw <chin.unisocial@gmail.com>
2014-05-18 21:33:12 +1000

    add logo, event relationships and navigations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+  belongs_to :clubs
+  has_many :users
+  validates :name , presence:true
+  validates :memberOnly , presence:true
+  validates :date , presence:true
+  validates :place , presence:true
 end
