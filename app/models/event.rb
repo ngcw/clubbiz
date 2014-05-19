fa20264fc6c9e291040374d62bd8c8b27dd21fdf
ngcw <chin.unisocial@gmail.com>
2014-05-19 14:44:37 +1000

    bla

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,10 @@ class Event < ActiveRecord::Base
   validates :memberOnly , presence:true
   validates :date , presence:true
   validates :place , presence:true
+  def self.save(upload)
+    name = upload['datafile'].original_filename
+    directory = "assets/club/data"
+    path = File.join(directory, name)
+    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
+  end
 end
