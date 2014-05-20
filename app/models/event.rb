77f462ea87d1829fd5e5582b761ebd91b7e32b60
Renlord <renlord.yang@gmail.com>
2014-05-21 09:57:22 +1000

    fixed event bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
 
   belongs_to :clubs
   has_many :users
-  has_and_belongs_to_many :sharedWithClubs, :class_name => 'Club', :foreien_key => 'shared_event_id'
+  has_and_belongs_to_many :sharedWithClubs, :class_name => 'Club', :foreign_key => 'shared_event_id'
   validates :name , presence:true
   validates :memberOnly , presence:true
   validates :date , presence:true
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # root_url definition
-  root to: "home#index"
+  root to: "clubs#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
