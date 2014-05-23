b346e1c487e781d3e691fe0ea3dcca6756986b2d
Renlord <renlord.yang@gmail.com>
2014-05-23 19:44:27 +1000

    removed user validation hceck

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,4 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :clubs
   has_and_belongs_to_many :events
   has_many :club_announcements
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :phone_number, presence: true
 end
