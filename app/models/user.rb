30431d2ab51b975610e7ea629917a59026c43b63
Renlord <renlord.yang@gmail.com>
2014-05-23 19:13:31 +1000

    updated user schema and prepared the register form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :events
   has_many :club_announcements
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
 end
